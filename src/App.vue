357d0b2a10856befdc2db2597ba94fe88631a5ee
Harsh Jain <harsh.jain@energyexemplar.com>
2024-08-10 21:15:07 +0530

    add previous typing history on profile page

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,20 @@
 <template>
-    <v-app class="h-100">
-        <top-nav-bar></top-nav-bar>
+    <v-app>
         <navigation-panels></navigation-panels>
         <v-main>
-            <v-container>
-                <router-view v-slot="{ Component }">
-                    <keep-alive>
-                        <component
-                            :is="Component"
-                            class="w-100 h-100"
-                            @init-map="() => {
-                                console.log('the map emitted something')
-                            }"
-                            @load-map="(instance) => {
-                                console.log('the map has been loaded emitted from style(dot)load', instance)
-                            }"
-                            @zoom="(zoom) => {
-                                console.log('zoom is changing', zoom)
-                            }"
-                        > </component>
-                    </keep-alive>
-                </router-view>
-            </v-container>
+            <router-view v-slot="{ Component }">
+                <keep-alive>
+                    <component
+                        :is="Component"
+                        class="w-100 h-100"
+                    > </component>
+                </keep-alive>
+            </router-view>
         </v-main>
     </v-app>
 </template>
 
 <script setup>
-import TopNavBar from './components/TopNavBar.vue'
 import NavigationPanels from './components/NavigationPanels.vue'
 import { onMounted, onActivated } from 'vue'
 import { useUserDetailsStore } from './stores/userDetails'
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,23 +1,47 @@
 <template>
-    Welcome {{ userName }}
+    <div class="d-flex flex-column w-100 h-100">
+        <div class="d-flex w-100 text-h6 justify-center">
+            Welcome {{ userName}}
+        </div>
+        <div class="d-flex w-100 h-100 py-5">
+            <div class="d-flex flex-column w-25 h-100">
+                <v-list>
+                    <v-list-group v-for="(date, index) in userSubmissionsByDate" :key="index">
+                        <template #activator="{ props }">
+                            <v-list-item v-bind="props">
+                                {{ date[0].submissionDate.split("T")[0] }}
+                            </v-list-item>
+                        </template>
+                        <v-list-item>
+                            <v-card class="d-flex w-100 flex-column elevation-10">
+                                <v-card-text>
+                                    <v-timeline side="end" class="w-100">
+                                        <v-timeline-item v-for="(submission, index) in date" :key="index" dot-color="pink" class="w-100">
+                                            <v-btn class="w-100" @click="setCurrSelectedSubmission(submission)">
+                                                <div class="text-h6">
+                                                    {{ submission.submissionDate.split("T")[1].split(".")[0]}}
+                                                </div>
+                                            </v-btn>
+                                        </v-timeline-item>
+                                    </v-timeline>
+                                </v-card-text>
+                            </v-card>
+                        </v-list-item>
+                    </v-list-group>
+                </v-list>
+            </div>                    
 
-    <v-list>
-        <v-list-group v-for="(submission, index) in userSubmissions" :key="index">
-            <template #activator="{ props }">
-                <v-list-item v-bind="props" :title="submission.submissionDate">
-                </v-list-item>
-            </template>
-
-            <div>
-                <typing-result class="w-50 h-50" :line-chart-data="submission.typingSpeedPerSecond" :raw-line-chart-data="submission.rawTypingSpeedPerSecond"></typing-result>
+            <div class="d-flex w-75 h-100 elevation-10 mx-5">
+                <typing-result v-if="currSelectedSubmission" :line-chart-data="currSelectedSubmission.typingSpeedPerSecond" :raw-line-chart-data="currSelectedSubmission.rawTypingSpeedPerSecond">
+                </typing-result>
             </div>
-        </v-list-group>
-    </v-list>
+        </div>
+    </div>
 </template>
 
 <script>
-import { defineComponent, onActivated, ref } from 'vue'
-import { useRoute } from 'vue-router';
+import { computed, defineComponent, nextTick, onActivated, ref, watch } from 'vue'
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 import TypingResult from '@/components/TypingResult.vue';
 
 export default defineComponent({
@@ -25,11 +49,27 @@ export default defineComponent({
     setup() {
         const route = useRoute();
         const userSubmissions = ref([]);
+        const currSelectedSubmission = ref()
 
-        const userName = route.params.username;
+        const userName = computed(() => route.params.username);
 
-        onActivated(async () => {
-            const call = await fetch(`https://canarytype-alpha3.azurewebsites.net/api/TypingArena/UserSubmission/${userName}`, {
+        const userSubmissionsByDate = computed(() => {
+            return userSubmissions.value.reduce((acc, current) => {
+                const date = current.submissionDate.split("T")[0];
+                if (!acc[date]) {
+                    acc[date] = [];
+                }
+                acc[date].push(current);
+                return acc;
+            }, {})
+        })
+
+        const setCurrSelectedSubmission = (submission) => {
+            currSelectedSubmission.value = submission;
+        }
+
+        const fetchUserSubmissionFromBackend = async () => {
+            const call = await fetch(`https://canarytype-alpha3.azurewebsites.net/api/TypingArena/UserSubmission/${userName.value}`, {
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8'
                 },
@@ -38,11 +78,22 @@ export default defineComponent({
             if (call.status === 200) {
                 userSubmissions.value = await call.json()
             }
+        }
+
+        onActivated(async () => {
+            fetchUserSubmissionFromBackend()
+        })
+
+        watch(userName, () => {
+            fetchUserSubmissionFromBackend()
         })
 
         return { 
             userName,
-            userSubmissions
+            userSubmissions,
+            userSubmissionsByDate,
+            currSelectedSubmission,
+            setCurrSelectedSubmission
         }
     },
 })
