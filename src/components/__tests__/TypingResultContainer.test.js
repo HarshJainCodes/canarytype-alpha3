91b00cb4eb7986fc468fb6974e83f1103eed9ec2
Harsh Jain <harsh.jain@energyexemplar.com>
2024-09-22 00:41:34 +0530

    setup few tests

---
+++ src/components/__tests__/TypingResultContainer.test.js
@@ -0,0 +1,93 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+
+import TypingResultContainer from "../TypingResultContainer.vue";
+import { setActivePinia, createPinia } from 'pinia'
+import { useUserDetailsStore } from "@/stores/userDetails";
+import { useToast } from "vue-toastification";
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+vi.mock("@/stores/userDetails")
+
+describe('TypingResultContainer.vue', () => {
+    let userDetailsStore;
+
+    beforeEach(() => {
+        userDetailsStore = {
+            isLoggedIn: false,
+            userName: 'testUser',
+            setIsLoggedIn: vi.fn(),
+        }
+        useUserDetailsStore.mockReturnValue(userDetailsStore)
+        setActivePinia(createPinia())
+    })
+
+    it('renders correctly', () => {
+        const wrapper = mount(TypingResultContainer, {
+            props: {
+                lineChartData: [1, 2 ,3],
+                rawLineChartData: [1, 2, 3],
+                typingSpeed: 100
+            },
+            global: {
+                plugins: [vuetify]
+            }
+        })
+
+        expect(wrapper.find('[data-qa-id="typing-result-container"]').exists()).toBe(true);
+        expect(wrapper.text()).toContain("Average Typing Speed");
+    })
+
+    it('should show red login banner if user is not logged in and reverse', () => {
+        userDetailsStore.isLoggedIn = false;
+        const wrapper = mount(TypingResultContainer, {
+            props: {
+                lineChartData: [1, 2 ,3],
+                rawLineChartData: [1, 2, 3],
+                typingSpeed: 100
+            },
+            global: {
+                plugins: [vuetify]
+            }
+        })
+
+        expect(wrapper.find('[data-qa-id="not-login-banner"]').exists()).toBeTruthy()
+
+        userDetailsStore.isLoggedIn = true;
+        const wrapper1 = mount(TypingResultContainer, {
+            props: {
+                lineChartData: [1, 2 ,3],
+                rawLineChartData: [1, 2, 3],
+                typingSpeed: 100
+            },
+            global: {
+                plugins: [vuetify]
+            }
+        })
+        expect(wrapper1.find('[data-qa-id="not-login-banner"]').exists()).toBeFalsy()
+    })
+
+    it('should emit update:typingFinished when button is clicked', async () => {
+        const wrapper = mount(TypingResultContainer, {
+            props: {
+                lineChartData: [1, 2 ,3],
+                rawLineChartData: [1, 2, 3],
+                typingSpeed: 100
+            },
+            global: {
+                plugins: [vuetify]
+            }
+        })
+
+        await wrapper.find('[data-qa-id="type-again-button"]').trigger('click')
+        expect(wrapper.emitted('update:typingFinished')).toBeTruthy();
+        expect(wrapper.emitted('update:typingFinished')[0]).toEqual([false])
+    })
+})
